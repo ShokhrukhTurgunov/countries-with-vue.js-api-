5d7ae2d6b535f60f0161c41a1ec2201a3b9ed2b9
ShokhrukhTurgunov <shohruhturgunov4991@gmail.com>
2021-10-13 17:57:24 +0500

    create country box with api

--- src/components/CountryCreate.vue
+++ src/components/CountryCreate.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <div v-for="country in countriesList" :key="country.id">
-      <img :src="country.flag" alt="">
-      <h1>{{ country.name }}</h1>
-      <p >{{country.name}}</p>
-      
+    <div class="container main-container">
+      <div class="country-box" v-for="country in countriesList.slice(0, 12)" :key="country.id">
+      <img :src="country.flag" alt="countries-flag" width="264" height="160">
+      <h3>{{ country.name }}</h3>
+      <p class="main-item-text">Population: {{ country.population }}</p>
+      <p class="main-item-text">Region: {{ country.region }}</p>
+      <p class="main-item-text">Capital: {{ country.capital }}</p> 
     </div>
-    
+    </div>
   </div>
 </template>
 
@@ -33,9 +35,9 @@ import axios from 'axios'
     // },
     created() {
         axios
-          .get('https://restcountries.com/v2/all')  // Does a get request
+          .get('https://restcountries.com/v2/all?limit=8')  // Does a get request
           .then(response => {
-            this.countriesList = response.data 
+            this.countriesList = response.data
             console.log(response.data);
           })
           .catch(error => {
@@ -46,5 +48,18 @@ import axios from 'axios'
 </script>
 
 <style scoped>
+.main-container{
+  padding: 0;
+  padding-top: 48px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+.country-box{
+  margin-top: 75px;
+  box-shadow: 0px 0px 7px 2px rgba(0, 0, 0, 0.0294384);
+  border-radius: 5px;
+}
 
 </style>
