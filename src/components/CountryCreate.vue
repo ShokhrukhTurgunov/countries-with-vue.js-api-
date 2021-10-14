52500608ab91c911429ffe09cd0ad913bba2bc46
ShokhrukhTurgunov <shohruhturgunov4991@gmail.com>
2021-10-14 07:53:33 +0500

    add style for country cards

--- src/components/CountryCreate.vue
+++ src/components/CountryCreate.vue
@@ -2,11 +2,11 @@
   <div>
     <div class="container main-container">
       <div class="country-box" v-for="country in countriesList.slice(0, 12)" :key="country.id">
-      <img :src="country.flag" alt="countries-flag" width="264" height="160">
-      <h3>{{ country.name }}</h3>
-      <p class="main-item-text">Population: {{ country.population }}</p>
-      <p class="main-item-text">Region: {{ country.region }}</p>
-      <p class="main-item-text">Capital: {{ country.capital }}</p> 
+      <img class="country-flag" :src="country.flag" alt="countries-flag" width="264" height="160">
+      <h3 class="country-name">{{ country.name }}</h3>
+      <p class="country-text">Population: <span>{{ country.population }}</span></p>
+      <p class="country-text">Region: <span>{{ country.region }}</span></p>
+      <p class="country-text">Capital: <span>{{ country.capital }}</span></p> 
     </div>
     </div>
   </div>
@@ -57,9 +57,48 @@ import axios from 'axios'
 }
 
 .country-box{
-  margin-top: 75px;
-  box-shadow: 0px 0px 7px 2px rgba(0, 0, 0, 0.0294384);
-  border-radius: 5px;
+   width: 264px;
+    padding: 0;
+    padding-bottom: 40px;
+    margin-bottom: 75px;
+    display: flex;
+    flex-direction: column;
+    background: #FFFFFF;
+    box-shadow: 0px 0px 7px 2px rgba(0, 0, 0, 0.0294384);
+    border-radius: 5px;
+    color: #2B3844;
+    transition: .5s ease;
+}
+
+.country-box:hover{
+  box-shadow: 0px 3px 2px #2222228f;
+}
+
+.country-flag{
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+
+.country-name{
+    margin: 24px 0 16px 24px;
+    font-weight: 800;
+    font-size: 18px;
+    line-height: 26px;
+}
+
+.country-text{
+    margin: 0;
+    margin-bottom: 8px;
+    margin-left: 24px;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 16px;
+}
+
+.country-text span{
+  font-weight: 300;
+  font-size: 14px;
+  line-height: 16px;
 }
 
 </style>
